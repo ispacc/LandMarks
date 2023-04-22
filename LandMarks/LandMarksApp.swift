fd1c1c75948b0d40de32bc309f117defa2df143c
Ispacc <yh.xion@outlook.com>
2023-04-22 22:26:06 +0800

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by 十四 on 2023/4/22.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
